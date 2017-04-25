42b6181fc056a85f7b2bdbedf0ae5d8bea709c23
ivchenkodima <ivchenkodima@gmail.com>
2017-04-25 14:15:36 +0300

    remove format fn

--- src/reduxFormValidate.js
+++ src/reduxFormValidate.js
@@ -6,21 +6,21 @@ import validate from './validate';
 export default (schema, { includeRequired = false } = {}) => (values, props) => {
   // FIXME: hotfix for redux form validation of Fields Array
   if (Array.isArray(values)) return undefined;
-  return validate(values, schema, {
-    props,
-    format: errors => Object.entries(errors).reduce((prev, [path, errorObj]) => {
-      const pathSchema = errorObj.schema;
-      const value = getFn(values, path);
+  const errors = validate(values, schema, { props });
 
-      if (includeRequired === false && pathSchema.required !== true && (value === '' || typeof value === 'undefined' || value === null)) {
-        return prev;
-      }
-      if (errorObj.isArray) {
-        setFn(prev, `${path}._error`, errorObj.error);
-      } else {
-        setFn(prev, path, errorObj.error);
-      }
+  return Object.entries(errors).reduce((prev, [path, errorObj]) => {
+    const pathSchema = errorObj.schema;
+    const value = getFn(values, path);
+
+    if (includeRequired === false && pathSchema.required !== true && (value === '' || typeof value === 'undefined' || value === null)) {
       return prev;
-    }, {})
-  });
+    }
+
+    if (errorObj.isArray) {
+      setFn(prev, `${path}._error`, errorObj.error);
+    } else {
+      setFn(prev, path, errorObj.error);
+    }
+    return prev;
+  }, {});
 };
