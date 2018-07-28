3ff3a11ccb25c9700235e7f2ae81c7838df2b057
Alexey Bondarenko <alexeybondarenko@me.com>
2018-07-29 01:57:20 +0300

    Update enzyme and react to 16

--- test/.setup.js
+++ test/.setup.js
@@ -1,5 +1,9 @@
 import { expect } from 'chai';
 import { jsdom } from 'jsdom';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 var exposedProperties = ['window', 'navigator', 'document'];
 
